2c6f2f00d736116c53a4e6cee1c6d191675c7f9a
Louise <95115391+Arbbos@users.noreply.github.com>
2024-12-04 19:58:58 +0800

    Add files via upload

---
+++ DB_MalangLouiseJoy_AndoyFateIris_BermasJamesBrian_DelosSantosDianne_ManguneXavierNash.sql
@@ -0,0 +1,137 @@
+CREATE DATABASE  IF NOT EXISTS `test` /*!40100 DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci */;
+USE `test`;
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: test
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.32-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `authentication`
+--
+
+DROP TABLE IF EXISTS `authentication`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `authentication` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `authentication`
+--
+
+LOCK TABLES `authentication` WRITE;
+/*!40000 ALTER TABLE `authentication` DISABLE KEYS */;
+INSERT INTO `authentication` VALUES ('admin','user'),('user','admin');
+/*!40000 ALTER TABLE `authentication` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `products` (
+  `product_id` int(11) NOT NULL,
+  `product_name` varchar(50) DEFAULT NULL,
+  `stock_levels` int(11) DEFAULT NULL,
+  `size` varchar(45) DEFAULT NULL,
+  `color` varchar(45) DEFAULT NULL,
+  `category` varchar(45) DEFAULT NULL,
+  `location` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `stock_movements`
+--
+
+DROP TABLE IF EXISTS `stock_movements`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `stock_movements` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_id` varchar(50) DEFAULT NULL,
+  `type` enum('RECEIPT','SHIPMENT') DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `comments` text DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stock_movements`
+--
+
+LOCK TABLES `stock_movements` WRITE;
+/*!40000 ALTER TABLE `stock_movements` DISABLE KEYS */;
+INSERT INTO `stock_movements` VALUES (1,'PT01','RECEIPT',20,'2024-11-24','Initial Restock'),(2,'PT02','SHIPMENT',10,'2024-11-24','Shipped for an order'),(3,'PT01','RECEIPT',20,'2024-11-24','Initial Restock'),(4,'PT02','SHIPMENT',10,'2024-11-24','Shipped for an order');
+/*!40000 ALTER TABLE `stock_movements` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `stockmovements`
+--
+
+DROP TABLE IF EXISTS `stockmovements`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `stockmovements` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_id` varchar(50) DEFAULT NULL,
+  `type` enum('RECEIPT','SHIPMENT') DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `comments` text DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stockmovements`
+--
+
+LOCK TABLES `stockmovements` WRITE;
+/*!40000 ALTER TABLE `stockmovements` DISABLE KEYS */;
+INSERT INTO `stockmovements` VALUES (1,'PT01','RECEIPT',20,'2024-11-24','Initial Restock'),(2,'PT02','SHIPMENT',10,'2024-11-24','Shipped for an order'),(3,'PT03','SHIPMENT',10,'2024-11-25','Shipped for an Order'),(4,'PT01','RECEIPT',30,'2024-11-25','Restocking'),(5,'PT01','SHIPMENT',50,'2024-11-27','Shipped for an order'),(6,'PT02','RECEIPT',50,'2024-11-27','Restocking'),(7,'PT033','SHIPMENT',50,'2024-12-01','FOR SHIPMENT');
+/*!40000 ALTER TABLE `stockmovements` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-12-04 19:58:22
